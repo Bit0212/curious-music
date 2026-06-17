4807268f696f5e9068676478a3f95997feb3917d
Bit0212 <andriuvipip@protonmail.com>
2026-06-16 23:29:22 -0400

    updated how does it process data

--- musica.py
+++ musica.py
@@ -1,30 +1,64 @@
 import numpy as np
 import sounddevice as sde
 import random
+import readchar
+from readchar import key
+
+notasportecla = {
+    'w': (261, 2, 44100),
+    'e': (294, 2, 44100)
+}
+
+
 def audiobit(freq,duration,freq_muestreo):
-    #freq = 440;
-    #duration =384.2;
-    #freq_muestreo = 44100;
+ 
     zeit = np.linspace(0, duration, int(freq_muestreo * duration), endpoint=False)
     sinus = 0.5*np.sin(2*np.pi*freq*zeit)
     print("ha comenzado el juego")
+    sde.stop()
     sde.play(sinus,freq_muestreo)
-    f = int(input("si te duelen los oidos presiona un boton para salir\n"))
-    if f<=1:
-        sde.stop()
-        return
-    sde.wait()
+   # tecla = readchar.readkey()
+#    if tecla:
+ #       sde.wait()
+  #      return
+
+    #sde.wait()
     print("hemos terminado tio")
+
+
+
+
+
+def keydetectaudio(y):
+    audiobit(*y)
+
+    
+
+
+
+
 def main():
     print("amigo ha comenzado una batalla sin cuartel porfavor estate atento")
-    while True:
-        f = random.randint(1,15)
+    
 
-        audiobit(random.randint(50,200),f,44100)
-        k = int(input("estoy a la espera de que me mandés algo jaja jeje jijji"))
-        audiobit(k,f,44100)
+    while True:
+        teclis = readchar.readkey()
+        if teclis  == key.ESC:
+            print("adios")
+            break
+        if teclis in notasportecla:
+            keydetectaudio(notasportecla[teclis])
+
+        
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
 
 
 
